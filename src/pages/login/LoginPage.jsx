d067f2dba8cddd8a695b2e9b8568bd301134d7e9
devi22002 <devi22002@mail.unpad.ac.id>
2025-12-14 18:51:13 +0700

    edit route after cust login

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -47,7 +47,7 @@ export default function LoginPage() {
         }, 1500);
       } else {
         setTimeout(() => {
-          navigate("/");
+          navigate("/catalog");
         }, 1500);
       }
     } catch (err) {
@@ -83,7 +83,7 @@ export default function LoginPage() {
         }, 1500);
       } else {
         setTimeout(() => {
-          navigate("/");
+          navigate("/catalog");
         }, 1500);
       }
     } catch (err) {
